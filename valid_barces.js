01298f098205454840cba2f8ecb4bbe11068f118
Amr <amrtaweel111@gmail.com>
2023-08-07 13:28:22 +0300

    Valid Barces challenge solution

---
+++ valid_barces.js
@@ -0,0 +1,17 @@
+function validBraces(braces){
+    const openBraces = ['{','(','[']
+    const closeBraces =['}',')',']']
+    let barcesOrder = false
+    for( let i = 1; i < braces.length; i++){
+      const firstBrace = braces[0]
+      let closeBrace =closeBraces[openBraces.indexOf(firstBrace)]
+      if( openBraces.includes(firstBrace) ==true && ( i - 1 )% 2 == 0 && closeBrace == braces[i]){
+        braces = braces.slice( 1, i ) + braces.slice( i+1 )
+        i=0
+      }
+    }
+    if( braces .length ==0 ){
+      barcesOrder = true
+    }
+    return barcesOrder
+}
